5cd0f4bd2661c3fc010b81c583b2d36db3dbb379
kry23 <32355427+kry23@users.noreply.github.com>
2020-11-10 11:49:13 +0300

    changes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    childrenCount: 0,
+    adultCount: 1,
+    childrenAge:0,
+    date1:'',
+    date2: {}
   },
   mutations: {
+    increment(state) {
+      state.childrenCount++
+    },
+    increment2(state) {
+      state.adultCount++
+    },
+    subtract(state) {
+      state.childrenCount = state.childrenCount - 1;
+    },
+    
+    subtract2(state) {
+      state.adultCount = state.adultCount - 1;
+    },
+    addAge(state) {
+      state.childrenAge++;
+     },
+    
+     subtractAge(state) {
+       state.childrenAge = state.childrenAge - 1;
+     }
+    // dateDivide(state) {
+    //   state.date2 = state.date1.split('~')
+    // }
   },
   actions: {
   },
