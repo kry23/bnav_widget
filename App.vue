082c5995b8077f736241dc1e3960056a51c5be37
kry23 <32355427+kry23@users.noreply.github.com>
2020-11-10 11:20:16 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,12 +1,31 @@
 <template>
-<div id="app">
+  <div id="app">
     <div id="nav">
-
+     <Reservation2></Reservation2>
     </div>
-    <router-view />
-</div>
+    <router-view/>
+  </div>
 </template>
+<script lang="ts">
+import Reservation2 from './components/Reservation2.vue';
+import {Component,Vue} from 'vue-property-decorator';
 
-<style lang="scss">
+
+@Component({
+  components:{
+    Reservation2
+  }
+
+})
+  export default class App extends Vue {
+    
+  }
+
+
+</script>
+
+
+<style>
+
 
 </style>
